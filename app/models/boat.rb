1499bd28411119393c87053ccedc232da0d5aaa7
Berry  Semexan <bsemexan@gmail.com>
2018-04-08 03:42:04 +0000

    automatically backed up by learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,26 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def first_five
+    self.first(5)
+  end
+
+  def dinghy
+  end
+
+  def ship
+  end
+
+  def last_three_alphabetically
+  end
+
+  def without_a_captain
+  end
+
+  def sailboats
+  end
+
+  def with_three_classification
+  end
 end
